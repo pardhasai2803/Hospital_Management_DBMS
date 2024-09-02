87338f63048267e35d25a2fab7843627008c9eb9
pardhasai2803 <pardhasai.inala@gmail.com>
2024-09-02 17:42:48 +0530

    first commit

---
+++ DataEntryOp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('login/', views.login_DataEntryOp, name='login'),
+    path('findpatient/', views.findpatient, name="findpatient"),
+    path('<str:patient_id>/<str:appid>/addTest/', views.updateTests, name="addTest"),
+    path('<str:patient_id>/<str:appid>/showTreatment/', views.updateTreatments, name="addTreatment"),
+    path('<str:patient_id>/<str:appid>/addAdmTreatment/', views.admTreatments, name="admTreatment"),
+    path('upload_file/', views.upload_file, name="upload_file"),
+]
